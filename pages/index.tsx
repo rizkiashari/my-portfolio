5e9506620d14352202229239ab0061e05d7ca6b5
RizkiAshari <rizkiashari1026@gmail.com>
2021-12-21 11:43:14 +0700

    add meta desc

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,21 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { gsap } from "gsap";
+import styles from "../styles/Home.module.css";
+import Navbar from "../components/Navbar";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      
-    </div>
-  )
-}
+    <>
+      <Head>
+        <title>Rizki Ashari | Home</title>
+      </Head>
+      <div className={styles.container}>
+        <Navbar />
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
